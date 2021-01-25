b8c36f44d827d6e6b8f8eb256acd141f2e7fb552
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-01-25 17:04:41 -0500

    Implemented ReactTypingEffect

--- src/Components/UI/Background/Background.js
+++ src/Components/UI/Background/Background.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import classes from './Background.module.css';
+import React from "react";
+import classes from "./Background.module.css";
+import ReactTypingEffect from "react-typing-effect";
 
 function Background() {
-    return (
-        <div className={classes.BackgroundContainer}>
-
-        </div>
-    )
+  return (
+    <div className={classes.BackgroundContainer}>
+      <h1>Hello, my name is Julio Rivas</h1>
+      <ReactTypingEffect
+        className={classes.TypingEffect}
+        staticText="I'm a"
+        text={[
+          "software developer",
+          "music enthusiast",
+          "technology nerd",
+        ]}
+        speed="100"
+        typingDelay="500"
+        eraseDelay="1000"
+      />
+    </div>
+  );
 }
 
-export default Background
+export default Background;
