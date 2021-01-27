60504ddc9a309559fd7e8de52d361a795ff120a7
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-01-26 23:56:34 -0500

    Implementing Modal component for projects page

--- src/Components/Pages/ProjectsPage/ProjectsPage.js
+++ src/Components/Pages/ProjectsPage/ProjectsPage.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react";
+import Modal from "../../Modal/Modal";
 
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
 
 function ProjectsPage() {
-    return (
-        <div id="ProjectsPage">
-            PROJECTS PAGE
-        </div>
-    )
+  return (
+    <div id="ProjectsPage">
+      <Container>
+        <Button>View Project</Button>
+      </Container>
+    </div>
+  );
 }
 
 export default ProjectsPage;
