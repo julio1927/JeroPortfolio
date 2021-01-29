bcf289d00f10b98e301d0cfdd6051ca421f38cc6
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-01-29 17:24:45 -0500

    Removed Modal Component

--- src/Components/Pages/ProjectsPage/ProjectsPage.js
+++ src/Components/Pages/ProjectsPage/ProjectsPage.js
@@ -1,42 +1,10 @@
 import React, { useState } from "react";
-import Modal from "../../Modal/Modal";
-import { GlobalStyle } from '../../UI/GlobalStyle/GlobalStyle';
-
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const Button = styled.button`
-  min-width: 100px;
-  padding: 16px 32px;
-  border-radius: 4px;
-  border: none;
-  background: #141414;
-  color: #fff;
-  font-size: 24px;
-  cursor: pointer;
-`;
 
 function ProjectsPage() {
 
-  const [showModal, setShowModal] = useState(false);
-
-  const openModal = () => {
-      setShowModal(prev => !prev);
-  }
-
   return (
     <div id="ProjectsPage">
-      <Container>
-        <Button onClick={openModal}>View Project</Button>
-        <Modal showModal={showModal} setShowModal={setShowModal}/>
-        <GlobalStyle />
-      </Container>
+        Projects Page
     </div>
   );
 }
