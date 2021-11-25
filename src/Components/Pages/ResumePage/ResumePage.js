6580badfd52b3becc977db68d475b62961da84ac
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-11-25 14:14:55 -0500

    Updated resume assets

--- src/Components/Pages/ResumePage/ResumePage.js
+++ src/Components/Pages/ResumePage/ResumePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./ResumePage.module.css";
-import ResumeImg from "../../../Assets/Images/Resume.PNG";
+import ResumeImg from "../../../Assets/Images/Resume1.PNG";
+import ResumeImg2 from "../../../Assets/Images/Resume2.PNG";
 
 function ResumePage() {
   return (
@@ -10,6 +11,9 @@ function ResumePage() {
         <div className={classes.ResumeWrapper}>
           <ul className={classes.ResumeItem}>
             <img src={ResumeImg} alt="Resume" />
+          </ul>
+          <ul className={classes.ResumeItem}>
+            <img src={ResumeImg2} alt="Resume" />
           </ul>
         </div>
       </div>
