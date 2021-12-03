721e8b4f7c0712d503b7f3d185e6b4cfd8d0f512
julio1927 <38108470+julio1927@users.noreply.github.com>
2021-12-03 16:17:40 -0500

    Card Structure

--- src/Components/AboutMeCard/AboutMeCard.js
+++ src/Components/AboutMeCard/AboutMeCard.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import AboutMeCardItem from '../AboutMeCardItem/AboutMeCardItem';
-import { AboutCardContainer, AboutCardBoarder } from './AboutMeCardElements';
+import React from "react";
+import AboutMeCardItem from "../AboutMeCardItem/AboutMeCardItem";
+import { AboutCardContainer, AboutCardBoarder } from "./AboutMeCardElements";
 
 const AboutMeCard = () => {
   return (
     <AboutCardContainer>
       <AboutCardBoarder>
-        <AboutMeCardItem title="Designer"/>
-        <AboutMeCardItem title="Front-End Developer"/>
-        <AboutMeCardItem title="Back-End Developer"/>
+        <AboutMeCardItem
+          Header="Designer"
+          HeaderInfoText="I value simple content structure, clean design patterns, and thoughtful interactions"
+          Subjects="Things I enjoy designing:"
+          SubjectText="UX, UI, Web, Apps, Logos"
+          Tools="Design Tools"
+          ToolsText="Figma, Sketch, Abobe XD, Pen & paper"
+        />
+        <AboutMeCardItem
+          Header="Full-Stack Developer"
+          HeaderInfoText="I value simple content structure, clean design patterns, and thoughtful interactions"
+          Subjects="Things I enjoy designing:"
+          SubjectText="UX, UI, Web, Apps, Logos"
+          Tools="Design Tools"
+          ToolsText="Figma, Sketch, Abobe XD, Pen & paper"
+        />
+        <AboutMeCardItem
+          Header="Freelance Developer"
+          HeaderInfoText="I value simple content structure, clean design patterns, and thoughtful interactions"
+          Subjects="Things I enjoy designing:"
+          SubjectText="UX, UI, Web, Apps, Logos"
+          Tools="Design Tools"
+          ToolsText="Figma, Sketch, Abobe XD, Pen & paper"
+        />
       </AboutCardBoarder>
     </AboutCardContainer>
-  )
-}
+  );
+};
 
-export default AboutMeCard
+export default AboutMeCard;
--- src/Components/AboutMeCardItem/AboutMeCardItem.js
+++ src/Components/AboutMeCardItem/AboutMeCardItem.js
@@ -7,36 +7,24 @@ const AboutMeCardItem = (props) => {
     <div>
       <Card className={classes.Card}>
         <Card.Body>
-          <Card.Title
-            className={classes.Title}
-            style={{ color: "#222831" }}
-            as="h1"
-          >
-            {props.title}
+          {/* Section Icon */}
+          <Card.Title className={classes.Header} as="h1">
+            {/* Section Title */}
+            {props.Header}
           </Card.Title>
-          <Card.Text className={classes.Text}>
-            <div>
-              <p>
-                Hello! I'm Julio, a software developer based in Kitchener, ON
-                who enjoys building things on the internet.
-              </p>
-              <p>
-                I strive to create clean and efficient code, with a good user
-                experience, which in turn helps me develop exceptional
-                applications.
-              </p>
+          <Card.Text className={classes.Text}>{props.HeaderInfoText}</Card.Text>
 
-              <p>
-                I recently graduated from{" "}
-                <a href="https://www.conestogac.on.ca">Conestoga College</a>,
-                where I was introduced to various technologies, concepts and
-                methods. I also had the opportunity to work on various semester
-                long projects with other software developers. I applied the
-                knowledge I learnt from school and also worked on some
-                freelancing projects.
-              </p>
-            </div>
-          </Card.Text>
+          {/* Section Title */}
+          <Card.Subtitle className={classes.Subtitle}>
+            {props.Subjects}
+          </Card.Subtitle>
+          <Card.Text className={classes.Text}>{props.SubjectText}</Card.Text>
+
+          {/* Section Title */}
+          <Card.Subtitle className={classes.Subtitle}>
+            {props.Tools}
+          </Card.Subtitle>
+          <Card.Text className={classes.Text}>{props.ToolsText}</Card.Text>
         </Card.Body>
       </Card>
     </div>
